926194a00da95b8eee6a32eeaf16135f72999e71
Olga Lutsyk <olyalutsyk@gmail.com>
2017-04-14 09:56:01 +0300

    Mock data

--- ShoppingList/ProductsViewController.swift
+++ ShoppingList/ProductsViewController.swift
@@ -19,11 +19,12 @@ class ProductsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Shop.sharedShop.stock = [StockItem(product:Product(id:0, title:"Product 1", price:10), quantity:2), StockItem(product:Product(id:1, title:"Product 2", price:15.55), quantity:2)]
+        Shop.sharedShop.stock = [StockItem(product:Product(id:0, title:"Product 1", price:10), quantity:2), StockItem(product:Product(id:1, title:"Product 2", price:15.55), quantity:2), StockItem(product:Product(id:2, title:"Product 3", price:10), quantity:2),StockItem(product:Product(id:3, title:"Product 4", price:10), quantity:2), StockItem(product:Product(id:4, title:"Product 5", price:10), quantity:2), StockItem(product:Product(id:5, title:"Product 6", price:10), quantity:2), StockItem(product:Product(id:6, title:"Product 7", price:10), quantity:2), StockItem(product:Product(id:7, title:"Product 8", price:10), quantity:2), StockItem(product:Product(id:8, title:"Product 9", price:10), quantity:2),StockItem(product:Product(id:9, title:"Product 10", price:10), quantity:2),StockItem(product:Product(id:10, title:"Product 11", price:10), quantity:2),StockItem(product:Product(id:11, title:"Product 12", price:10), quantity:2),StockItem(product:Product(id:12, title:"Product 13", price:10), quantity:2),StockItem(product:Product(id:13, title:"Product 14", price:10), quantity:2),StockItem(product:Product(id:14, title:"Product 15", price:10), quantity:2),StockItem(product:Product(id:15, title:"Product 16", price:10), quantity:2),StockItem(product:Product(id:16, title:"Product 17", price:10), quantity:2),StockItem(product:Product(id:17, title:"Product 18", price:10), quantity:2),StockItem(product:Product(id:18, title:"Product 19", price:10), quantity:2), StockItem(product:Product(id:19, title:"Product 20", price:10), quantity:2)]
         
         
         buttonWithBadge.setHandler(callback: showCart)
         buttonWithBadge.setImage(image: UIImage(named: "Cart")!)
+        buttonWithBadge.setTitle(value: "hbh")
         self.navigationItem.setRightBarButton(buttonWithBadge.getBarButtonItem(), animated: true)
     }
     
